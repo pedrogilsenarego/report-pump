08b06811b7c54b898769ffeb8a07feda6409906c
Pedro Rego <pedrogilsenarego@gmail.com>
2024-10-21 12:35:30 +0100

    a

--- components/ui/country-selector.tsx
+++ components/ui/country-selector.tsx
@@ -45,6 +45,7 @@ function CountrySelect({
     setCountries(
       filterCountries(countryRegionData, priorityOptions, whitelist, blacklist)
     );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
