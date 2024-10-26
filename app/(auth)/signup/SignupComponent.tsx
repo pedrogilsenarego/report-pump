5544a8b16e0cb59b4048bd8d1d889622e21dbe2a
Pedro Rego <pedrogilsenarego@gmail.com>
2024-10-26 09:31:46 +0100

    a

--- app/(auth)/signup/SignupComponent.tsx
+++ app/(auth)/signup/SignupComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { Button } from "@/components/ui/button";
@@ -24,213 +25,271 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import CountrySelect from "@/components/ui/country-selector";
-//import SSO from "@/modules/Auth/SSO";
+import { useState } from "react";
 
 export default function SignupComponent() {
   const { form, onSubmit, isPending } = useSignupComponent();
+  const [step, setStep] = useState(1);
   return (
-    <div className="w-1/2 h-screen rounded-3xl border p-32 space-y-5 relative bg-background flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold text-center">Join EquiTotal</h1>
-      {/* <SSO />
-      <div className="flex gap-4 items-center w-full">
-        <Divider />
-        <p className="text-nowrap text-slate-200 text-xs">
-          Or Create New Account
-        </p>
-        <Divider />
-      </div> */}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="w-full h-screen bg-primary p-10">
+          <div className="rounded-3xl border p-10 space-y-5 relative bg-background  flex flex-col h-full  min-h-full">
+            <h1 className="text-4xl font-bold text-center">Join Equitotal</h1>
+            <div className="min-h-fit flex-1">
+              <div className="flex flex-col h-full justify-between">
+                <div className="p-32 flex flex-col justify-center items-center h-full">
+                  {step === 1 && (
+                    <FormField
+                      control={form.control}
+                      name="role"
+                      render={({ field }) => (
+                        <FormItem className="space-y-3">
+                          <FormControl>
+                            <RadioGroup
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}
+                              className="flex flex-col space-y-6"
+                            >
+                              <FormItem className="flex items-center space-x-3 space-y-0">
+                                <FormControl>
+                                  <RadioGroupItem
+                                    className="h-8 w-8"
+                                    value="2"
+                                  />
+                                </FormControl>
+                                <FormLabel className="font-bold text-4xl cursor-pointer">
+                                  Are you a Customer?
+                                </FormLabel>
+                              </FormItem>
+                              <FormItem className="flex items-center space-x-3 space-y-0">
+                                <FormControl>
+                                  <RadioGroupItem
+                                    className="h-8 w-8"
+                                    value="3"
+                                  />
+                                </FormControl>
+                                <FormLabel className="font-bold text-4xl cursor-pointer">
+                                  Are you a Supplyer?
+                                </FormLabel>
+                              </FormItem>
+                            </RadioGroup>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  )}
+                  {step === 2 && (
+                    <div className="flex flex-col align-top space-y-8 ">
+                      <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-2xl">
+                              What is your username?
+                            </FormLabel>
+                            <FormControl>
+                              <Input placeholder="Username" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
 
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-5 w-full"
-        >
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Username" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="E-mail" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-2xl">
+                              And your email?
+                            </FormLabel>
+                            <FormControl>
+                              <Input placeholder="E-mail" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                  )}
+                  {step === 3 && (
+                    <div className="flex flex-col align-top space-y-8">
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-2xl">
+                              Choose a password
+                            </FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder="Password"
+                                {...field}
+                                type="password"
+                              />
+                            </FormControl>
+                            <p className="text-xs mt-2">
+                              Your password must be at least 8 characters long
+                            </p>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
 
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Password" {...field} type="password" />
-                </FormControl>
-                <p className="text-xs mt-2">
-                  Your password must be at least 8 characters long
-                </p>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="confirmPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    placeholder="Confirm Password"
-                    {...field}
-                    type="password"
-                  />
-                </FormControl>
+                      <FormField
+                        control={form.control}
+                        name="confirmPassword"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-2xl">
+                              Confirm the password
+                            </FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder="Confirm Password"
+                                {...field}
+                                type="password"
+                              />
+                            </FormControl>
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Separator />
-          <FormField
-            control={form.control}
-            name="role"
-            render={({ field }) => (
-              <FormItem className="space-y-3">
-                <FormLabel>Type of user</FormLabel>
-                <FormControl>
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                    className="flex flex-col space-y-1"
-                  >
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="2" />
-                      </FormControl>
-                      <FormLabel className="font-normal">Customer</FormLabel>
-                    </FormItem>
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="3" />
-                      </FormControl>
-                      <FormLabel className="font-normal">Supplyer</FormLabel>
-                    </FormItem>
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="nameCompany"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Name of the company" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="address"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Address" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="country"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl {...field}>
-                  <CountrySelect />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Phone/Celular" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="defaultLang"
-            render={({ field }) => (
-              <FormItem>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Default language for reports" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    {langKeys.map((lang) => {
-                      return (
-                        <SelectItem key={lang.id} value={lang.id}>
-                          {lang.value}
-                        </SelectItem>
-                      );
-                    })}
-                  </SelectContent>
-                </Select>
-                <FormDescription>
-                  This language will be used to create the reports, but can be
-                  changed later.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Separator />
-          <p className="text-xs mt-2">
-            By signining up you agree to our{" "}
-            <u className="cursor-pointer">Terms of Service</u> &{" "}
-            <u className="cursor-pointer">Privacy Policy</u>
-          </p>
-          <Button
-            isLoading={isPending}
-            className=" w-full flex items-center"
-            variant="default"
-            type="submit"
-          >
-            Agreed and Signup
-          </Button>
-        </form>
-      </Form>
-
-      <div className="glowbox -z-10"></div>
-    </div>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                  )}
+                  {step === 4 && (
+                    <>
+                      <FormField
+                        control={form.control}
+                        name="nameCompany"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <Input
+                                placeholder="Name of the company"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="address"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <Input placeholder="Address" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="country"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl {...field}>
+                              <CountrySelect />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <Input placeholder="Phone/Celular" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="defaultLang"
+                        render={({ field }) => (
+                          <FormItem>
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}
+                            >
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder="Default language for reports" />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {langKeys.map((lang) => {
+                                  return (
+                                    <SelectItem key={lang.id} value={lang.id}>
+                                      {lang.value}
+                                    </SelectItem>
+                                  );
+                                })}
+                              </SelectContent>
+                            </Select>
+                            <FormDescription>
+                              This language will be used to create the reports,
+                              but can be changed later.
+                            </FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <Separator />
+                      <p className="text-xs mt-2">
+                        By signining up you agree to our{" "}
+                        <u className="cursor-pointer">Terms of Service</u> &{" "}
+                        <u className="cursor-pointer">Privacy Policy</u>
+                      </p>
+                    </>
+                  )}
+                </div>
+                <div className="w-full flex justify-end space-x-2">
+                  {step > 1 && (
+                    <Button
+                      type="button"
+                      variant="outline"
+                      onClick={() => setStep((prev) => prev - 1)}
+                    >
+                      Previous
+                    </Button>
+                  )}
+                  {step !== 4 && (
+                    <Button
+                      type="button"
+                      onClick={() => setStep((prev) => prev + 1)}
+                      disabled={step === 1 && form.watch("role") === undefined}
+                    >
+                      Next
+                    </Button>
+                  )}
+                  {step === 4 && (
+                    <Button
+                      isLoading={isPending}
+                      variant="default"
+                      type="submit"
+                    >
+                      Agreed and Signup
+                    </Button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Form>
   );
 }
